6ce79a3cefadc0efcef2b40faa646e4ce9871e6a
GENERALOV <vg135@mail.ru>
2024-01-29 15:05:29 +0500

    added text.txt

--- js/main.js
+++ js/main.js
@@ -27,13 +27,12 @@ const swiper = new Swiper(".swiper", {
   const modalBlack = document.querySelector(".modal-black");
   const btnModal = document.querySelector(".control__button");
   const btnFooter = document.querySelector(".footer__button");
-  
 
   btnModal.addEventListener("click", openModal);
   btnFooter.addEventListener("click", openFooter);
   modal.addEventListener("click", closeModal);
   modalBlack.addEventListener("click", closeFooter);
-  
+
   function openModal(e) {
     e.preventDefault();
     document.body.classList.toggle("modal--open");
@@ -54,17 +53,13 @@ const swiper = new Swiper(".swiper", {
       document.body.classList.remove("modal--open-black");
     }
   }
-  
-  
-
 
   // ACCARDEON===============================
 
-
   const accardeons = document.querySelectorAll(".accordeon");
 
-  accardeons.forEach(el => {
-    el.addEventListener('click', (e) => {
+  accardeons.forEach((el) => {
+    el.addEventListener("click", (e) => {
       const self = e.currentTarget;
       const content = self.querySelector(".accordeon__content");
 
@@ -79,15 +74,18 @@ const swiper = new Swiper(".swiper", {
 
     //  TIMER=====================================
 
-
-    document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener("DOMContentLoaded", function () {
       // конечная дата, например 1 июля 2021
       const deadline = new Date(2024, 2, 4);
       // id таймера
       let timerId = null;
       // склонение числительных
       function declensionNum(num, words) {
-        return words[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(num % 10 < 5) ? num % 10 : 5]];
+        return words[
+          num % 100 > 4 && num % 100 < 20
+            ? 2
+            : [2, 0, 1, 1, 1, 2][num % 10 < 5 ? num % 10 : 5]
+        ];
       }
       // вычисляем разницу дат и устанавливаем оставшееся времени в качестве содержимого элементов
       function countdownTimer() {
@@ -98,45 +96,54 @@ const swiper = new Swiper(".swiper", {
         const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
         const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
         const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
-        $hours.textContent = hours < 10 ? '0' + hours : hours;
-        $minutes.textContent = minutes < 10 ? '0' + minutes : minutes;
-        $seconds.textContent = seconds < 10 ? '0' + seconds : seconds;
-        $hours.dataset.title = declensionNum(hours, ['час', 'часа', 'часов']);
-        $minutes.dataset.title = declensionNum(minutes, ['минута', 'минуты', 'минут']);
-        $seconds.dataset.title = declensionNum(seconds, ['секунда', 'секунды', 'секунд']);
+        $hours.textContent = hours < 10 ? "0" + hours : hours;
+        $minutes.textContent = minutes < 10 ? "0" + minutes : minutes;
+        $seconds.textContent = seconds < 10 ? "0" + seconds : seconds;
+        $hours.dataset.title = declensionNum(hours, ["час", "часа", "часов"]);
+        $minutes.dataset.title = declensionNum(minutes, [
+          "минута",
+          "минуты",
+          "минут",
+        ]);
+        $seconds.dataset.title = declensionNum(seconds, [
+          "секунда",
+          "секунды",
+          "секунд",
+        ]);
       }
       // получаем элементы, содержащие компоненты даты
-      const $hours = document.querySelector('.timer__hours');
-      const $minutes = document.querySelector('.timer__minutes');
-      const $seconds = document.querySelector('.timer__seconds');
+      const $hours = document.querySelector(".timer__hours");
+      const $minutes = document.querySelector(".timer__minutes");
+      const $seconds = document.querySelector(".timer__seconds");
       // вызываем функцию countdownTimer
       countdownTimer();
       // вызываем функцию countdownTimer каждую секунду
       timerId = setInterval(countdownTimer, 1000);
     });
-
   });
 
-// маска телефона
-$('.modal__tel-black').mask('+7 (999) 999-99-99');
-$('.modal__tel').mask('+7 (999) 999-99-99');
+  // маска телефона ================================
+
+  $(".modal__tel-black").mask("+7 (999) 999-99-99");
+  $(".modal__tel").mask("+7 (999) 999-99-99");
+
+  $.fn.setCursorPosition = function (pos) {
+    if ($(this).get(0).setSelectionRange) {
+      $(this).get(0).setSelectionRange(pos, pos);
+    } else if ($(this).get(0).createTextRange) {
+      var range = $(this).get(0).createTextRange();
+      range.collapse(true);
+      range.moveEnd("character", pos);
+      range.moveStart("character", pos);
+      range.select();
+    }
+  };
+
+  $('input[type="tel"]').click(function () {
+    $(this).setCursorPosition(4); // set position number
+  });
+  //  Блокировка сдвига контента=====================================
 
 
-$.fn.setCursorPosition = function (pos) {
-  if ($(this).get(0).setSelectionRange) {
-    $(this).get(0).setSelectionRange(pos, pos);
-  } else if ($(this).get(0).createTextRange) {
-    var range = $(this).get(0).createTextRange();
-    range.collapse(true);
-    range.moveEnd('character', pos);
-    range.moveStart('character', pos);
-    range.select();
-  }
-};
-
-
-$('input[type="tel"]').click(function () {
-  $(this).setCursorPosition(4);  // set position number
-});
-
+  
 })();
